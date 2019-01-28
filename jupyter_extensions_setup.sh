17359f2809ecc53a5ffc6b3bba1b8bd82843904b
patrickryan <theyoungsoul@gmail.com>
2019-01-27 22:34:56 -0500

    initial

---
+++ jupyter_extensions_setup.sh
@@ -0,0 +1,4 @@
+pip install jupyter_nbextensions_configurator jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+jupyter nbextensions_configurator enable --user
+
---
+++ setup.sh
@@ -0,0 +1,8 @@
+pip install --upgrade pip
+pip install scipy
+pip install scikit-learn
+pip install jupyter
+pip install matplotlib
+pip install pandas
+pip install seaborn
+
